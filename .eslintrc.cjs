376f00b6be22c7165bb662eeebf1bb65ed9d17b2
m-chanikya <pji.chanikya.m@kalvium.community>
2025-10-15 23:37:15 +0530

    adding eslint and husky

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.eslint.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+    ecmaVersion: 2020,
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  env: {
+    node: true,
+    jest: true,
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'no-console': ['error'],
+  },
+  ignorePatterns: ['dist', 'node_modules', '.eslintrc.cjs'],
+};
